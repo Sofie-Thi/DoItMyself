e4bb681122dc09b4419b7ed98279b97317400c6c
Sofie-Thi <sofiethi@stud.ntnu.no>
2026-01-02 18:52:34 +0100

    to do list

--- main.go
+++ main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello Go World!\n")
+	var shortGolang = "Watch Go crash course"
+	var fullGolang = "Watch Nana's Golang Full Course"
+	rewardDessert := "Reward myself with a donut"
+
+	fmt.Println("###### Welcome to our Todolist App! ######")
+
+	fmt.Println("List of my Todos")
+	fmt.Println(shortGolang)
+	fmt.Println(fullGolang)
+	fmt.Println(rewardDessert)
+
+	fmt.Println()
+	fmt.Println("Tutorials")
+	fmt.Println(shortGolang)
+	fmt.Println(fullGolang)
+
+	fmt.Println()
+	fmt.Println("Rewards")
+	fmt.Println(rewardDessert)
+
+	fmt.Println()
+	fmt.Println("My Project")
+	fmt.Println(fullGolang)
 }
